57e6b9075da584b897b899db673dafb82375b484
cholodymedia <cholodymedia@gmail.com>
2019-11-05 22:06:45 +0100

    Add fontawesome

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navi/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Navi from '@/components/Navi.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    Navi,
   },
 };
 </script>
 
 <style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100vw;
+  height: auto;
+  min-height: 100vh;
+  background-color: whitesmoke;
 }
 </style>
---
+++ src/components/Navi.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="navi">
+    <div class="logo-box">
+      <img :src="logo" alt="logo">
+    </div>
+    <div class="counter-box">
+      <div class="counter"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import image from '@/assets/logo.png';
+
+export default {
+  name: 'Navi',
+  data() {
+    return {
+      logo: image,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.navi {
+  width: 100%;
+  height: 10vh;
+  background-color: white;
+  display: flex;
+  .logo-box {
+    width: 10vw;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    img {
+      width: 40%;
+    }
+  }
+  .counter-box {
+    width: 12vw;
+    height: 100%;
+    margin-left: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .counter {
+      width: 100px;
+      height: 50px;
+      border-radius: 10px;
+      background-color: whitesmoke;
+    }
+  }
+}
+</style>
